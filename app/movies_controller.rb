452368c3760b7aa75cc35788bee8301e235d9dbc
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-07-17 16:24:56 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,7 +23,7 @@ def can_be_created_with_a_hash_of_attributes
   movie
 end
 
-def can_be_created_in_a_block(args = {})
+def can_be_created_in_a_block
   movie = Movie.create do |m|
     m.title = title
     m.release_date = year

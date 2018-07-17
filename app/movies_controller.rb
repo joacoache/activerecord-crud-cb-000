d525cb16e9f2b36d94ec7b47a64d8cf21c55511f
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-07-17 16:10:02 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -19,8 +19,8 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = Movie.new(attributes)
-  movie.save
+  movie = Movie.find_or_create_by(attributes)
+  movie
 end
 
 def can_be_created_in_a_block(args = __)
